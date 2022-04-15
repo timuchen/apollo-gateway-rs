d649c24b094caa94c0986950bef128cae3ac5508
K0R0VA <yarykov.m@gmail.com>
2022-04-15 14:41:26 +0300

    вернул старые константы

--- crates/handler/src/constants.rs
+++ crates/handler/src/constants.rs
@@ -1,10 +1,10 @@
 use opentelemetry::Key;
 
-pub const KEY_SERVICE: Key = Key::from_static_str("graphql-gateway.service");
-pub const KEY_QUERY: Key = Key::from_static_str("graphql-gateway.query");
-pub const KEY_PATH: Key = Key::from_static_str("graphql-gateway.path");
-pub const KEY_PARENT_TYPE: Key = Key::from_static_str("graphql-gateway.parentType");
-pub const KEY_RETURN_TYPE: Key = Key::from_static_str("graphql-gateway.returnType");
-pub const KEY_FIELD_NAME: Key = Key::from_static_str("graphql-gateway.fieldName");
-pub const KEY_VARIABLES: Key = Key::from_static_str("graphql-gateway.variables");
-pub const KEY_ERROR: Key = Key::from_static_str("graphql-gateway.error");
+pub const KEY_SERVICE: Key = Key::from_static_str("graphgate.service");
+pub const KEY_QUERY: Key = Key::from_static_str("graphgate.query");
+pub const KEY_PATH: Key = Key::from_static_str("graphgate.path");
+pub const KEY_PARENT_TYPE: Key = Key::from_static_str("graphgate.parentType");
+pub const KEY_RETURN_TYPE: Key = Key::from_static_str("graphgate.returnType");
+pub const KEY_FIELD_NAME: Key = Key::from_static_str("graphgate.fieldName");
+pub const KEY_VARIABLES: Key = Key::from_static_str("graphgate.variables");
+pub const KEY_ERROR: Key = Key::from_static_str("graphgate.error");
